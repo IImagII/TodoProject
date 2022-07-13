9e5f9783627cc672a511be20e05b30512a21face
iimagii <mag_1@ukr.net>
2022-07-13 18:28:24 +0300

    add fucntion render and deleteTodo

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ function addTodo(text) {
    };
    todos.push(todo);
 }
-console.log(todos);
+
+function deleteTodo(id) {
+   todos.forEach((todo) => {
+      if (todo.id === id) {
+         todo.isDone = true;
+      }
+   });
+}
+
+function render() {
+   console.log(todos);
+}
+addTodo('пробная');
+render();
